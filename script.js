9b073a0eeb6a32f79f72661916f72f67731df84d
Benjamin <Tsubarah@hotmail.se>
2021-11-15 12:40:51 +0100

    updated array

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const students = [
  {
    name: "A",
-   img: "", 
+   image: "/images/benjamin_T.jpg",
+   show: false 
  },
  {
   name: "B",
-  img: "", 
+  image: "", 
+  show: false
 },
 {
   name: "C",
-  img: "", 
+  image: "", 
+  show: false
+},
+{
+  name: "D",
+  image: "", 
+  show: false
 }
 ];
 
@@ -17,10 +25,32 @@ const guessImage = document.querySelector('.guessImage');
 const profileImage = document.querySelector('.profileImage');
 const btns = document.querySelectorAll('.btn');
 
+let guessName;
+let guessedName;
 
+  // 1. Select person that has not been shown before and show their image
+  
+  
+
+  
+
+  //create array of students names
+  // const studentsNames = students.map(student => {
+  //   return student.name;
+  // });
+
+  // console.log('Student names:', studentsNames);
 
 btns.forEach(btn => {
+  // 2. Pick random name
+  // 3. Set name in data-attribute
+
   btn.addEventListener('click', e => {
+    e.preventDefault;
+  // 4. If random name is equal to image name = set green border else red border
+  // 5. Start timer (2-3 sec)
+  // 6. reset(clear everything);
+  // 7. init();
     console.log(e.target.classList);
   })
 
